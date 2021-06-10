f253cd9f7cada5766bec9cef6f220e317dc588d1
Oleg Rodzewich <oleg_rodzevich@epam.com>
2021-06-10 10:52:18 +0300

    Clock

--- docs/flow.js
+++ docs/flow.js
@@ -34,18 +34,24 @@ const flowConfig = {
     ]
 };
 
+const today = new Date();
 const flowName = window.location.hash ? window.location.hash.replace(/^#(?:.+:)?/, '') : undefined;
 const currentFlow = flowName ? flowConfig[flowName] : undefined;
 const scriptName = location.pathname.split('/').pop().split('.').shift();
 
 if (currentFlow) {
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    const key = `${scriptName}_${yyyy}-${mm}-${dd}`;
+    const value  = parseInt(localStorage.getItem(key) || '0', 10) || 0;
     const currentConfig = currentFlow.find(item => item.name === scriptName);
     const nextConfig = currentConfig ? currentFlow[currentFlow.indexOf(currentConfig) + 1] : undefined;
     if (currentConfig) {
         setTimeout(() => {
             const preparedHash = currentConfig.hash ? `${currentConfig.hash}:` : '';
             window.location.href = nextConfig ? `./${nextConfig.name}.html#${preparedHash}${flowName}` : './';
-        }, currentConfig.time);
+        }, Math.max(0, currentConfig.time - value));
     } else {
         window.location.href = './';
     }
