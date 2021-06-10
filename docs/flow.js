97337eef81c5122408e948474b40ecdd8f2799c9
Oleg Rodzewich <oleg_rodzevich@epam.com>
2021-06-10 10:42:13 +0300

    Clock

---
+++ docs/flow.js
@@ -0,0 +1,52 @@
+const flowConfig = {
+    'training1': [
+        {
+            name: 'warm-up',
+            time: 3 * 60 * 1000
+        },
+        {
+            name: 'clock',
+            time: 2 * 60 * 1000
+        },
+        {
+            name: 'earthquake',
+            time: 10 * 60 * 1000
+        },
+        {
+            name: 'lode-runner',
+            hash: '1',
+            time: 10 * 60 * 1000
+        },
+        {
+            name: 'colors',
+            hash: '1',
+            time: 5 * 60 * 1000
+        },
+        {
+            name: 'schulte-table',
+            hash: '1',
+            time: 10 * 60 * 1000
+        },
+        {
+            name: 'stat',
+            time: 2 * 60 * 1000
+        }
+    ]
+};
+
+const flowName = window.location.hash ? window.location.hash.replace(/^#(?:.+:)?/, '') : undefined;
+const currentFlow = flowName ? flowConfig[flowName] : undefined;
+const scriptName = location.pathname.split('/').pop().split('.').shift();
+
+if (currentFlow) {
+    const currentConfig = currentFlow.find(item => item.name === scriptName);
+    const nextConfig = currentConfig ? currentFlow[currentFlow.indexOf(currentConfig) + 1] : undefined;
+    if (currentConfig) {
+        setTimeout(() => {
+            const preparedHash = currentConfig.hash ? `${currentConfig.hash}:` : '';
+            window.location.href = nextConfig ? `./${nextConfig.name}.html#${preparedHash}${flowName}` : './';
+        }, currentConfig.time);
+    } else {
+        window.location.href = './';
+    }
+}
